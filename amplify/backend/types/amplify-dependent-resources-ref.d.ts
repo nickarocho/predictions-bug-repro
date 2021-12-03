795ba5c01cada7d793b99e288c39ac79d8975046
Nick Arocho <nicaroch@amazon.com>
2021-12-03 10:01:17 -0800

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "predictionsbugreprofcd0da02": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "translateTexta5eede85": {
+            "region": "string",
+            "sourceLang": "string",
+            "targetLang": "string"
+        },
+        "speechGenerator9c69933d": {
+            "region": "string",
+            "language": "string",
+            "voice": "string"
+        }
+    }
+}
